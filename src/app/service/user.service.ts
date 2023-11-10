3fdfae1c9145cbba9544f4a52769f597e0c8dc30
khanhnq <quockhanhnguyen2882@gmail.com>
2023-11-10 17:35:55 +0700

    upload avatar

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UpdateProfileRequest } from '../request/update-profile.model';
 
-const USER_API = 'http://localhost:8080/user/';
+const USER_API = 'http://localhost:8080/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -17,7 +17,17 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public updateProfile(updateProfileRequest: UpdateProfileRequest) : Observable<any>{
-    return this.httpClient.post(USER_API+'update-profile', updateProfileRequest, httpOptions);
+    return this.httpClient.post(USER_API+'user/update-profile', updateProfileRequest, httpOptions);
+  }
+
+  public uploadAvatar(avatar : File) : Observable<HttpEvent<any>>{
+    const formData: FormData = new FormData();
+    formData.append('avatar', avatar);
+    const req = new HttpRequest('POST', USER_API+'user/upload-avatar', formData, {
+      reportProgress: true,
+      responseType: 'json',
+    });
+    return this.httpClient.request(req);
   }
   
 }
--- src/app/update-profile/update-profile.component.ts
+++ src/app/update-profile/update-profile.component.ts
@@ -10,10 +10,10 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-update-profile',
   templateUrl: './update-profile.component.html',
-  styleUrls: ['./update-profile.component.css']
+  styleUrls: ['./update-profile.component.css'],
 })
-export class UpdateProfileComponent implements OnInit{
-  @ViewChild('updateProfileForm', {static: false}) updateProfileForm !: NgForm;
+export class UpdateProfileComponent implements OnInit {
+  @ViewChild('updateProfileForm', { static: false }) updateProfileForm!: NgForm;
 
   isSuccessful = false;
   isFailed = false;
@@ -22,9 +22,16 @@ export class UpdateProfileComponent implements OnInit{
   cities: City[] = [];
   districts: District[] = [];
   wards: Ward[] = [];
-  id : number = 0;
+  id: number = 0;
+  selectedFiles?: FileList;
+  currentFile?: File;
+  message = '';
+  preview = '';
 
-  constructor(private httpClient: HttpClient, private userService: UserService) {}
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.getCities().subscribe((result: City[]) => {
@@ -32,42 +39,55 @@ export class UpdateProfileComponent implements OnInit{
     });
   }
 
-  getCities():Observable<City[]> {
-    return this.httpClient.get<City[]>('http://localhost:8080/address/cities', )
-     .pipe(map(response=> {
-        if(response){
-           return Object.values(response);
-         }
-         return [];
-     }));
+  getCities(): Observable<City[]> {
+    return this.httpClient
+      .get<City[]>('http://localhost:8080/address/cities')
+      .pipe(
+        map((response) => {
+          if (response) {
+            return Object.values(response);
+          }
+          return [];
+        })
+      );
   }
 
   changeCity(e: any) {
-    this.httpClient.get<District[]>('http://localhost:8080/address/districts/'+e.target.value, )
-     .pipe(map(response=> {
-        if(response){
-           return Object.values(response);
-         }
-         return [];
-     })).subscribe((result: District[]) => {
-      this.districts = result;
-    });
+    this.httpClient
+      .get<District[]>(
+        'http://localhost:8080/address/districts/' + e.target.value
+      )
+      .pipe(
+        map((response) => {
+          if (response) {
+            return Object.values(response);
+          }
+          return [];
+        })
+      )
+      .subscribe((result: District[]) => {
+        this.districts = result;
+      });
     this.wards = [];
   }
 
   changeDistrict(e: any) {
-    this.httpClient.get<Ward[]>('http://localhost:8080/address/wards/'+e.target.value, )
-     .pipe(map(response=> {
-        if(response){
-           return Object.values(response);
-         }
-         return [];
-     })).subscribe((result: Ward[]) => {
-      this.wards = result;
-    });
+    this.httpClient
+      .get<Ward[]>('http://localhost:8080/address/wards/' + e.target.value)
+      .pipe(
+        map((response) => {
+          if (response) {
+            return Object.values(response);
+          }
+          return [];
+        })
+      )
+      .subscribe((result: Ward[]) => {
+        this.wards = result;
+      });
   }
 
-  changeWard(e : any) {
+  changeWard(e: any) {
     this.id = e.target.value;
   }
 
@@ -79,19 +99,66 @@ export class UpdateProfileComponent implements OnInit{
       gender: this.updateProfileForm.value.gender,
       phoneNumber: this.updateProfileForm.value.phoneNumber,
       specificAddress: this.updateProfileForm.value.specificAddress,
-      wardId : this.id
-    }
+      wardId: this.id,
+    };
     this.userService.updateProfile(updateProfileRequest).subscribe({
-      next : data => {
+      next: (data) => {
         this.isSuccessful = true;
         this.successMessage = data.message;
-      }, 
-      error: err => {
+        this.reloadPage();
+      },
+      error: (err) => {
         this.isFailed = true;
         this.errorMessage = err.error.message;
-      }
-    })
-
+      },
+    });
   }
 
+  selectFile(event: any): void {
+    this.message = '';
+    this.preview = '';
+    this.selectedFiles = event.target.files;
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      if (file) {
+        this.preview = '';
+        this.currentFile = file;
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+          this.preview = e.target.result;
+        };
+        reader.readAsDataURL(this.currentFile);
+      }
+    }
+  }
+
+  upload() {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      if (file) {
+        this.currentFile = file;
+        this.userService.uploadAvatar(this.currentFile).subscribe({
+          next: (event: any) => {
+            this.reloadPage();
+            // this.successMessage = event.target.value;
+          },
+          error: (err: any) => {
+            console.log(err);
+            if (err.error && err.error.message) {
+              this.errorMessage = err.error.message;
+            } else {
+              this.errorMessage = 'Could not upload the image!';
+            }
+            this.currentFile = undefined;
+          },
+        });
+      }
+      this.selectedFiles = undefined;
+    }
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
 }
