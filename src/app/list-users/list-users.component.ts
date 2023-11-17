8751b6bc6c471c1878740f801c812c7f210a4718
khanhnq <quockhanhnguyen2882@gmail.com>
2023-11-17 13:46:36 +0700

    list users

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -10,23 +10,12 @@ import { AdminService } from '../service/admin.service';
 })
 export class ListUsersComponent implements OnInit{
   listUsers: UserResponse[] = [];
-  userGender: string = '';
-  userAddress: string = '';
-  userRoles: string = '';
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.getAllUsers().subscribe((result: UserResponse[]) => {
-      this.listUsers = result;
-      for (let user of this.listUsers) {
-        if (user.gender == 1) {
-          this.userGender = 'Male';
-        } 
-        this.userGender = 'Female';
-        this.userAddress = user.specificAddress+', '+user.wardName+', '+user.districtName+', '+user.cityName;
-        this.userRoles = user.roles.join(', ');
-      }     
+      this.listUsers = result;   
     });
   }
 
--- src/app/response/user-response.model.ts
+++ src/app/response/user-response.model.ts
@@ -1,4 +1,5 @@
 export class UserResponse {
+    userId !: string;
     userCode !: string;
     email !: string;
     firstName !: string;
