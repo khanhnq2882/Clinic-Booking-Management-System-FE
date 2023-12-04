5f2a0e66e7b18a3f524149d291181f17aaf105c4
khanhnq <quockhanhnguyen2882@gmail.com>
2023-12-04 08:46:11 +0700

    fix work schedule

--- src/app/doctor/update-doctor-profile/update-doctor-profile.component.ts
+++ src/app/doctor/update-doctor-profile/update-doctor-profile.component.ts
@@ -29,17 +29,25 @@ export class UpdateDoctorProfileComponent {
   errorMessage = '';
   successMessage = '';
   workSchedulesDTO : WorkScheduleDTO[] = [
-    new WorkScheduleDTO('08:00', '09:00'),
-    new WorkScheduleDTO('09:00', '10:00'),
-    new WorkScheduleDTO('10:00', '11:00'),
-    new WorkScheduleDTO('11:00', '12:00'),
-    new WorkScheduleDTO('13:00', '14:00'),
-    new WorkScheduleDTO('14:00', '15:00'),
-    new WorkScheduleDTO('15:00', '16:00'),
-    new WorkScheduleDTO('16:00', '17:00'),
-    new WorkScheduleDTO('17:00', '18:00'),
-    new WorkScheduleDTO('18:00', '19:00'),
-    new WorkScheduleDTO('19:00', '20:00'),
+    new WorkScheduleDTO('08:00', '08:30'),
+    new WorkScheduleDTO('08:30', '09:00'),
+    new WorkScheduleDTO('09:00', '09:30'),
+    new WorkScheduleDTO('09:30', '10:00'),
+    new WorkScheduleDTO('10:30', '11:00'),
+    new WorkScheduleDTO('11:00', '11:30'),
+    new WorkScheduleDTO('11:30', '12:00'),
+    new WorkScheduleDTO('13:30', '14:00'),
+    new WorkScheduleDTO('14:00', '14:30'),
+    new WorkScheduleDTO('14:30', '15:00'),
+    new WorkScheduleDTO('15:00', '15:30'),
+    new WorkScheduleDTO('13:30', '16:00'),
+    new WorkScheduleDTO('16:30', '17:00'),
+    new WorkScheduleDTO('17:00', '17:30'),
+    new WorkScheduleDTO('17:30', '18:00'),
+    new WorkScheduleDTO('18:00', '18:30'),
+    new WorkScheduleDTO('18:30', '19:00'),
+    new WorkScheduleDTO('19:00', '19:30'),
+    new WorkScheduleDTO('19:30', '20:00')
   ];
 
   workSchedules : WorkScheduleDTO[] = [];
